95c0415cc67a6a094ff536176c013b49dd82c931
Ioanna Dimitriou <ioanna.m.dimitriou@gmail.com>
2017-04-07 00:21:49 +0200

    Visualises dependencies in asd files

---
+++ README.md
@@ -0,0 +1,45 @@
+
+# ASD-GRAPH
+
+This small utility uses Graphviz Draw to visualise the dependencies
+declared in a Common Lisp `<package-name>.asd` file.
+
+## Usage
+
+Load this file in a Common Lisp REPL (tested with CCL and SBCL), 
+and if <file-name> is your path (including the path to the file)
+to your <package-name>.asd, evaluate:
+
+`(asd-graph <file-name>)`
+
+or
+
+`(asd-graph <file-name> :output-dir <output-dir>)`
+
+This will create a dot and a pdf file in the same directory as 
+<file-name> or in <output-dir> respectively. 
+
+If you prefer a different output (png, svg, <etc>), then run from 
+the same directory in a terminal the following command:
+
+dot -T<etc> <package-name>.dot -o <output-file-name>
+
+where <etc> is the ending of your preferred format.
+
+### Formats supported by dot:
+
+canon, dot, fig, gd, gif, hpgl, imap, cmap, mif, mp, pcl, pic,
+plain (ascii), png, ps, ps2, svg, vrml, vtx, wbmp.
+
+According to the graphviz manual: 
+http://www.graphviz.org/Documentation/dotguide.pdf
+
+
+
+## Requirements
+
+* Linux OS
+
+* Graphviz (dot should be in `/usr/bin/dot`)
+
+* Quicklisp
---
+++ asd-graph.lisp
@@ -0,0 +1,143 @@
+#|
+
+# ASD-GRAPH
+
+
+This utility uses Graphviz Draw to visualise the dependencies
+declared in a Common Lisp `<package-name>.asd` file.
+
+See REAMDE.md file for more details.
+
+|#
+
+;; # Code
+
+(ql:quickload :maxpc)
+(ql:quickload :external-program)
+
+;; ## Parsing
+
+(defun =text-until (parser)
+  "The input, which `parser` parses is not consumed."
+  (maxpc:=subseq (maxpc:%some (maxpc:?not parser))))
+
+(defun %skip-up-to-string (string)
+  (maxpc:=destructure (_ _)
+      (maxpc:=list (=text-until #1=(maxpc.char:?string string))
+		   #1#)))
+
+(defun %quote ()
+  (maxpc.char:?string "\""))
+
+(defun %skip-whitespace ()
+  (maxpc:%any
+    (maxpc.char:?whitespace)))
+
+(defun =quoted-word ()
+  (maxpc:=destructure (_ list _)
+      (maxpc:=list (%skip-whitespace)
+		   (maxpc:=subseq
+		    (maxpc:?seq (%quote)
+				(=text-until (%quote))
+				(%quote)))
+		   (%skip-whitespace))))
+
+(defun =in-parens (parser)
+  (maxpc:=destructure (_ content _)
+      (maxpc:=list (maxpc.char:?string "(")
+		   parser
+		   (maxpc.char:?string ")"))))
+
+(defun =dep ()
+  (maxpc:=destructure (_ file deps _)
+      (maxpc:=list (%skip-up-to-string ":file")
+		   (=quoted-word)
+		   (maxpc:%maybe
+		    (maxpc:=list
+		     (maxpc.char:?string ":depends-on ")
+		     (%skip-whitespace)
+		     (=in-parens (maxpc:%some (=quoted-word)))))
+		   (maxpc.char:?string ")"))
+    (list file (third deps))))
+  
+(defun =deps ()
+  (maxpc:%some (=dep)))
+
+;; ## Extracting dependencies
+;;
+;; References to \"packages\" are removed.
+
+(defun get-deps (file-name)
+  (with-open-file (in file-name
+		      :direction :input
+		      :if-does-not-exist :error)
+    (maxpc:parse in (=deps))))
+
+(defun packages-string-p (string)
+  (equal string "\"packages\""))
+
+(defun deps (file-name)
+  (loop for (file deps) in (get-deps file-name)
+     for clean-deps = (loop for d in deps
+			 unless (packages-string-p d)
+			 collect d)
+     unless (packages-string-p file)
+     collect (list file clean-deps)))
+  
+;; ## Manipulating file and path names
+
+(defun split-path (file-name)
+  (let ((name-start (+ 1 (search "/" file-name :from-end T))))
+    (list
+     (subseq file-name 0 name-start)
+     (subseq file-name name-start (- (length file-name) 4)))))
+
+(defun filename (path name ending)
+  (concatenate 'string
+	       path
+	       name
+	       "."
+	       ending))
+
+;; ## Outputting dot syntax
+
+(defvar *dot-settings*
+  "splines=ortho;~%rankdir = LR;~%node [shape=box];")
+
+(defvar *dot-ending*
+  "}")
+
+(defun dot-beginning (package-name)
+  (format nil "digraph ~a {" package-name))
+
+(defun dep->dot (dep)
+  (destructuring-bind (file dep-list) dep
+    (labels ((format% (d)
+	     (format nil "~a -> ~a;" file d)))
+      (format nil "~{~a~%~}" (mapcar #'format% dep-list)))))
+
+(defun asd->dot (path file-name package-name)
+    (with-open-file (out (filename path package-name "dot")
+			 :direction :output
+			 :if-exists :supersede)
+      (format out (dot-beginning package-name))
+      (format out *dot-settings*)
+      (loop for dep in (deps file-name)
+	 do (format out (dep->dot dep)))
+      (format out *dot-ending*)))
+
+;; ## The main function
+
+(defun asd-graph (file-name &key (output-dir nil))
+  (destructuring-bind
+	(path package-name) (split-path file-name)
+    (unless output-dir
+      (setf output-dir path))
+    (asd->dot output-dir file-name package-name)
+    (external-program:run
+     "/usr/bin/dot"
+     (list
+      "-Tpdf"
+      (filename output-dir package-name "dot")
+      "-o"
+      (filename output-dir package-name "pdf")))))
