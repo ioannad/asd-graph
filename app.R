21b03b7a1176c2b8cca5a6594cf745cafc39c10a
Ioanna Dimitriou <ioanna.m.dimitriou@gmail.com>
2018-05-30 01:35:26 +0200

    wip

--- app.R
+++ app.R
@@ -1,23 +1,50 @@
 require(stringi)
 require(shiny)
+require(DiagrammeR)
 
 ui <- fluidPage(h1("asd-graph in R"),
                 p("This small utility uses graphviz dot to visualise the dependencies declared in a Common Lisp <system-name>.asd file."),
                 fileInput(inputId="asd",
                           label="Upload your *.asd file here.",
-                          accept=".asd"),
+                          accept=".dot"),
                 textOutput(outputId="test",
                            container=div,
-                           inline=false)
+                           inline=false),
+                grVizOutput("graph")
                 )
 
 server <- function(input, output) {
-    output$test <- renderText({
-        file <- input$asd
-        rawText <- readLines(file$datapath)
-        com <- paste0("sbcl --load asd-graph.lisp --eval \'(asd-graph \"~/quicklisp/local-projects/jeffrey/jeffrey.asd\" :output-dir \"~/quicklisp/local-projects/asd-graph/\")\'")
-        system(com)
-        })
+    output$graph <- renderDiagrammeR(
+        DiagrammeR::grViz(diagram = "
+digraph 0 {
+splines=ortho;
+rankdir = LR;
+node [shape=box];\"read\" -> \"graph\";
+\"read\" -> \"parse\";
+\"read\" -> \"process-strings\";
+\"predicates\" -> \"graph\";
+\"predicates\" -> \"read\";
+\"draw\" -> \"graph\";
+\"draw\" -> \"read\";
+\"draw\" -> \"predicates\";
+\"labelmaker\" -> \"graph\";
+\"labelmaker\" -> \"read\";
+\"main\" -> \"graph\";
+\"main\" -> \"read\";
+\"main\" -> \"predicates\";
+\"main\" -> \"draw\";
+\"latex-in-html\" -> \"graph\";
+\"web-graph\" -> \"main\";
+\"website\" -> \"parse-web-input\";
+\"website\" -> \"latex-in-html\";
+\"website\" -> \"encode-filenames\";
+\"example-form-groups\" -> \"main\";
+\"test\" -> \"graph\";
+\"test\" -> \"read\";
+\"test\" -> \"predicates\";
 }
-
+"))
+}
+    
+    
 shinyApp(ui=ui, server=server)
