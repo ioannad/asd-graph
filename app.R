d89dd3634e2c843b0645fa84d844d7d3c7b737a4
Ioanna Dimitriou <ioanna.m.dimitriou@gmail.com>
2018-05-30 18:11:49 +0200

    almost ready - one substitution left

--- app.R
+++ app.R
@@ -6,45 +6,27 @@ ui <- fluidPage(h1("asd-graph in R"),
                 p("This small utility uses graphviz dot to visualise the dependencies declared in a Common Lisp <system-name>.asd file."),
                 fileInput(inputId="asd",
                           label="Upload your *.asd file here.",
-                          accept=".dot"),
-                textOutput(outputId="test",
-                           container=div,
-                           inline=false),
+                          accept=".asd"),
+                textOutput(outputId="test", container=div),
                 grVizOutput("graph")
                 )
 
 server <- function(input, output) {
-    output$graph <- renderDiagrammeR(
-        DiagrammeR::grViz(diagram = "
-digraph 0 {
-splines=ortho;
-rankdir = LR;
-node [shape=box];\"read\" -> \"graph\";
-\"read\" -> \"parse\";
-\"read\" -> \"process-strings\";
-\"predicates\" -> \"graph\";
-\"predicates\" -> \"read\";
-\"draw\" -> \"graph\";
-\"draw\" -> \"read\";
-\"draw\" -> \"predicates\";
-\"labelmaker\" -> \"graph\";
-\"labelmaker\" -> \"read\";
-\"main\" -> \"graph\";
-\"main\" -> \"read\";
-\"main\" -> \"predicates\";
-\"main\" -> \"draw\";
-\"latex-in-html\" -> \"graph\";
-\"web-graph\" -> \"main\";
-\"website\" -> \"parse-web-input\";
-\"website\" -> \"latex-in-html\";
-\"website\" -> \"encode-filenames\";
-\"example-form-groups\" -> \"main\";
-\"test\" -> \"graph\";
-\"test\" -> \"read\";
-\"test\" -> \"predicates\";
+
+    asd <- reactive(input$asd)
+    output$graph <- renderDiagrammeR({
+        evalexpression <- paste0("'(asd->dot \"",
+                                 "~/quicklisp/local-projects/jeffrey/jeffrey.asd",
+                                 "\" *standard-output*)'")
+        dottext <- system2('sbcl',
+                           args=c("--noinform", "--load", "\"asd-graph.lisp\"",
+                                  "--eval", evalexpression, "--eval", "'(quit)'"),
+                           stdout = TRUE)
+        DiagrammeR::grViz(diagram = dottext)
+    })
 }
-"))
-}
+                  
+    
     
     
 shinyApp(ui=ui, server=server)
